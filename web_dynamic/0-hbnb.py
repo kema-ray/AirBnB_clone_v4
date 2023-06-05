e41b803cbf6cbf72fb8794bbe4d8108b3da8eaa0
kema-ray <rachelkemuma99@gmail.com>
2023-06-05 05:17:45 +0300

    fix code

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -37,10 +37,10 @@ def hbnb():
 
     cache_id = uuid.uuid4()
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places
+                           places=places,
                            cache_id=cache_id)
 
 
--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -8,9 +8,8 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -37,7 +36,7 @@ def hbnb():
 
     cache_id = uuid.uuid4()
 
-    return render_template('100-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,

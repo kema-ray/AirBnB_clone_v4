53f842870210366a6b45d4480e22e7641f04a0f3
kema-ray <rachelkemuma99@gmail.com>
2023-06-05 05:09:09 +0300

    fix code

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -40,7 +40,7 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places
+                           places=places,
                            cache_id=cache_id)
 
 
